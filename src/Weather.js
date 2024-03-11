b75d464c1e319ddc34b6a109f9ec2ccac5780f77
Ehsaan <ec22881@qmul.ac.uk>
2024-03-11 12:40:02 +0000

    got api working

--- src/Weather.js
+++ src/Weather.js
@@ -1,42 +1,59 @@
-// Weather.js
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Weather = () => {
-  const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
 
-  const apiKey = '0eb6fccdfb4dc326cd05c87f99c91444';
-
-  const getWeatherData = async () => {
+  const fetchData = async () => {
     try {
       const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=0eb6fccdfb4dc326cd05c87f99c91444`
       );
       setWeatherData(response.data);
+      console.log(response.data); //You can see all the weather data in console log
     } catch (error) {
-      console.error('Error fetching weather data:', error);
+      console.error(error);
     }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
   };
 
   return (
     <div>
-      <h1>Weather App</h1>
-      <input
-        type="text"
-        placeholder="Enter city"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <button onClick={getWeatherData}>Get Weather</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Get Weather</button>
+      </form>
 
-      {weatherData && (
-        <div>
-          <h2>{weatherData.name}, {weatherData.sys.country}</h2>
-          <p>{weatherData.weather[0].description}</p>
+      {weatherData ? (
+        <>
+          <h2>{weatherData.name}</h2>
           <p>Temperature: {weatherData.main.temp}°C</p>
-          <p>Humidity: {weatherData.main.humidity}%</p>
-        </div>
+          <p>Description: {weatherData.weather[0].description}</p>
+          <p>Feels like : {weatherData.main.feels_like}°C</p>
+          <p>Humidity : {weatherData.main.humidity}%</p>
+          <p>Pressure : {weatherData.main.pressure}</p>
+          <p>Wind Speed : {weatherData.wind.speed}m/s</p>
+        </>
+      ) : (
+        <p>Loading weather data...</p>
       )}
     </div>
   );
