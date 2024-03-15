515a27ca5b0181f14da016e72d61df84c67c6cec
Karun233 <karunravikumar23@gmail.com>
2024-03-15 00:00:47 +0000

    changes

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,19 @@
 // Header.js
 import './Header.css';
-function Header(){
-return (
-  <header className="header">
-    <div className="header-left">
-      <div className="hamburger-menu">
-        {/* Hamburger menu icon */}
-        <div className="bar"></div>ó
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-    </div>
-  </header>
-);
-};
 
-export default Header
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-left">
+        <div className="hamburger-menu">
+          {/* Hamburger menu icon */}
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
